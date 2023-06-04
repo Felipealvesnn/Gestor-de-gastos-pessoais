091190f72bc3c6cdf80a000d23a64e22d647a242
Felipe "Home <felipe.rudra@hotmail.com>
2023-06-04 11:31:57 -0300

    Adicionar arquivos de projeto.

---
+++ Gestor de gastos pessoais/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Gestor_de_gastos_pessoais.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
